09297dc098450fbfca6d8f7ca4124d0fd01d3d48
lp20010415 <2385034562@qq.com>
2021-07-03 20:18:07 +0800

    添加了新内容

---
+++ 修改/晓星晨曦2021.7.3/pythonFiles/html.py
@@ -0,0 +1,46 @@
+import json
+
+from django.db import connection
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from DataAction.Action import simplify
+from match.settings import BASE_DIR
+
+
+def test(request):
+    return render(request, 'test.html')
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def UploadFile(request):
+    return render(request, "file_operation/UploadFile.html")
+
+
+def DataSheet(request):
+    return render(request, "file_charts/DataSheet.html")
+
+
+def BarCharts(request):
+    return render(request, "file_charts/BarCharts.html")
+
+
+def GetData(request):
+    cursor = connection.cursor()
+    cursor.execute('select count(*) from milion_data')
+    count = cursor.fetchall()
+    cursor.execute('select * from milion_data limit 50 offset 0')
+    row = cursor.fetchall()
+    data = simplify(row)
+    giveBack = {
+        "code": 0,
+        "msg": "",
+        "count": count[0][0],
+        "data": data
+    }
+    return render(request, 'GetData.html', {'data': 'alert("哈")'})
+    # return render(request, 'GetData.html', {'data': json.dumps(giveBack, ensure_ascii=False)})
